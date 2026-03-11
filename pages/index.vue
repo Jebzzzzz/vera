5a1c20eac39621f6c6aa51425f7222376e3c8716
Jebron Ortula <24-1-01666@vsu.edu.ph>
2026-03-11 20:19:58 +0800

    [feat]Add event listeners and state management for cookie popup

--- pages/index.vue
+++ pages/index.vue
@@ -31,6 +31,60 @@ useHead({
  {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Serif+Display:ital,wght@0,100..900;1,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poiret+One&display=swap'}
  ]
 });
+
+const globalDelay = 500;
+  const allowCookies = useCookie<boolean>("allowCookies", {
+    sameSite: "none",
+    secure: true,
+    maxAge: 60 * 60 * 24,
+  });
+  allowCookies.value = allowCookies.value || false;
+  async function loadModal() {
+  const cookieModal = document.getElementById("vrCookieModal") as HTMLDivElement;
+  const cookieBox = document.getElementById("vrCookieBox") as HTMLDivElement;
+  const cookieX = document.getElementById("vrCookieX") as HTMLDivElement;
+  const cookieOK = document.getElementById("vrCookieOK") as HTMLDivElement;
+  if ( !cookieModal || !cookieBox || !cookieX || !cookieOK ) return;
+  if (allowCookies.value === true) {
+  allowAllCookies();
+    return;
+  }
+  showCookiePopup();
+  cookieX.addEventListener("click", (e) => {
+    showCookiePopup(false);
+  });
+  cookieOK.addEventListener("click", (e) => {
+    allowAllCookies();
+  });
+  }
+  async function showCookiePopup(show: boolean = true) {
+    const cookieBox = document.getElementById("vrCookieBox") as HTMLDivElement;
+    const cookieModal = document.getElementById("vrCookieModal") as HTMLDivElement;
+    if (!cookieBox || !cookieModal) {
+      setTimeout(showCookiePopup, 50);
+      return;
+    }
+    if (!show) {
+      cookieBox.classList.add("vr-hidden");
+      cookieModal.classList.add("vr-hidden");
+      return;
+    }
+
+    cookieBox.classList.remove("vr-hidden");
+    cookieModal.classList.remove("vr-hidden");
+  }
+  let lastCookieClicked = 0;
+  async function allowAllCookies() {
+    if (lastCookieClicked >= Date.now() - globalDelay) return;
+    lastCookieClicked = Date.now();
+    allowCookies.value = true;
+    showCookiePopup(false);
+  }
+  onMounted(() => {
+    setTimeout(() => {
+      loadModal();
+    }, 1);
+  });
 </script>
 <style scoped></style>
 <style></style>
